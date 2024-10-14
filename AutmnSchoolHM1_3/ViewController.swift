503f37ff9b5f23e884800c3e92e092f7f6d86b13
DaniilSukhanov <82699248+DaniilSukhanov@users.noreply.github.com>
2024-10-14 21:52:39 +0300

    Initial Commit

---
+++ AutmnSchoolHM1_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutmnSchoolHM1_3
+//
+//  Created by Даниил Суханов on 14.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
